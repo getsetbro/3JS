788c22ae75218b0064a03dc36e8fbecd9fc1a2eb
getsetbro <getsetbro@users.noreply.github.com>
2020-04-03 16:54:31 -0400

    v2

---
+++ lighting/script.js
@@ -0,0 +1,67 @@
+import * as THREE from 'three';
+import {TimelineMax, Expo} from 'gsap';
+let scene = new THREE.Scene();
+let wInnerWidth = window.innerWidth;
+let wInnerHeight = window.innerHeight;
+// PerspectiveCamera ost closely mimics human eye
+let camera = new THREE.PerspectiveCamera(50, wInnerWidth / wInnerHeight, 0.1, 1000);
+camera.position.z = 11;
+// RENDERERS: WEBGL, CSS2D, CSS3D, SVG
+let renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setClearColor("#ddd");
+renderer.setSize(wInnerWidth, wInnerHeight);
+let raycaster = new THREE.Raycaster();
+let mouse = new THREE.Vector2();
+let geometry = new THREE.BoxGeometry(1, 1, 1);
+let material = new THREE.MeshLambertMaterial({ color: 0xF7F7F7 });
+//let mesh = new THREE.Mesh(geometry, material);
+//scene.add(mesh);
+var i = 10;
+while (i--) {
+  let mesh = new THREE.Mesh(geometry, material);
+  mesh.position.x = (Math.random() - 0.5) * 10;
+  mesh.position.y = (Math.random() - 0.5) * 10;
+  mesh.position.z = (Math.random() - 0.5) * 10;
+  scene.add(mesh);
+}
+// lots of kinds of lights
+let light = new THREE.PointLight(0xFFFFFF, 1, 1000);
+light.position.set(0, 0, 0);
+scene.add(light);
+
+let light2 = new THREE.PointLight(0xFFFFFF, 2, 1000);
+light2.position.set(0, 0, 25);
+scene.add(light2);
+
+document.body.appendChild(renderer.domElement);
+
+window.addEventListener('resize', () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+});
+
+function repo(e) {
+  e.preventDefault();
+  mouse.x = e.clientX / window.innerWidth * 2 - 1;
+  mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+  raycaster.setFromCamera(mouse, camera);
+  let intersects = raycaster.intersectObjects(scene.children, true);
+  for (var i = 0; i < intersects.length; i++) {
+    this.tl = new TimelineMax();
+    this.tl.to(intersects[i].object.scale, 1, { x: 2, ease: Expo.easeOut });
+    this.tl.to(intersects[i].object.scale, .5, { x: .5, ease: Expo.easeOut });
+    this.tl.to(intersects[i].object.position, .5, { x: 2, ease: Expo.easeOut });
+    this.tl.to(intersects[i].object.rotation, .5, { y: Math.PI * .5, ease: Expo.easeOut }, "=-1.5");
+  }
+}
+
+window.addEventListener('click', repo);
+
+let render = function () {
+  // create a loop to redraw upon refresh; pauses when in other tabs
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+};
+
+render();
---
+++ objects/script.js
@@ -0,0 +1,95 @@
+import * as THREE from 'three';
+
+    // Set the scene size.
+    const WIDTH = window.innerWidth;
+    const HEIGHT = window.innerHeight;
+
+    // Set some camera attributes.
+    const VIEW_ANGLE = 45;
+    const ASPECT = WIDTH / HEIGHT;
+    const NEAR = 0.1;
+    const FAR = 10000;
+
+    // Get the DOM element to attach to
+    const container = document.querySelector("#container");
+
+    // Create a WebGL renderer, camera
+    // and a scene
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    // const camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+    let camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+
+    const scene = new THREE.Scene();
+
+    // Add the camera to the scene.
+    scene.add(camera);
+
+    // Start the renderer.
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(WIDTH / 1.5, HEIGHT / 1.5);
+
+    // Attach the renderer-supplied
+    // DOM element.
+    container.appendChild(renderer.domElement);
+
+    // create a point light
+    const pointLight = new THREE.PointLight(0xffffff);
+
+    // set its position
+    pointLight.position.x = 55;
+    pointLight.position.y = 50;
+    pointLight.position.z = 44;
+
+    // add to the scene
+    scene.add(pointLight);
+
+    // create the sphere's material
+    const sphereMaterial = new THREE.MeshLambertMaterial({ color: 0xcc00cc });
+
+    // Set up the sphere vars
+    const RADIUS = 3;
+    const SEGMENTS = 16;
+    const RINGS = 16;
+
+    // Create a new mesh with
+    // sphere geometry - we will cover
+    // the sphereMaterial next!
+    const sphere = new THREE.Mesh(
+      new THREE.SphereGeometry(RADIUS, SEGMENTS, RINGS),
+      sphereMaterial
+    );
+
+    // Move the Sphere back in Z so we
+    // can see it.
+    sphere.position.z = -2;
+
+    // Finally, add the sphere to the scene.
+    scene.add(sphere);
+
+    var geometry = new THREE.BoxGeometry(3, 3, 2);
+    var material = new THREE.MeshLambertMaterial({ color: 0xccffcc });
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+    var animate = function () {
+      requestAnimationFrame(animate);
+
+      cube.rotation.x += 0.005;
+      cube.rotation.y += 0.001;
+
+      renderer.render(scene, camera);
+    };
+
+    animate();
+
+    // function update() {
+    //   // Draw!
+    //   renderer.render(scene, camera);
+
+    //   // Schedule the next frame.
+    //   requestAnimationFrame(update);
+    // }
+
+    // // Schedule the first frame.
+    // requestAnimationFrame(update);

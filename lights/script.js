d91d8dd613f6f9eccf4a715b58c124c9a0f22207
getsetbro <getsetbro@users.noreply.github.com>
2020-04-06 11:14:35 -0400

    snowpackd

--- lights/script.js
+++ lights/script.js
@@ -1,5 +1,6 @@
-import * as THREE from 'three';
-import {TimelineMax, Expo} from 'gsap';
+import * as THREE from '../web_modules/three.js';
+import {TimelineMax, Expo} from '../web_modules/gsap.js';
+
 let scene = new THREE.Scene();
 let wInnerWidth = window.innerWidth;
 let wInnerHeight = window.innerHeight;

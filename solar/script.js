0a44f6bb17226e9407f564bdee67c35c14bb0463
sethb <getsetbro@users.noreply.github.com>
2020-04-17 16:23:40 -0400

    gaming

--- solar/script.js
+++ solar/script.js
@@ -5,6 +5,7 @@ import { OrbitControls } from "../../jsm/controls/OrbitControls.js";
 var controls;
 var container;
 var camera, cameraTarget, scene, renderer;
+let mouse = new THREE.Vector2();
 
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
@@ -14,9 +15,9 @@ function onWindowResize() {
 function init() {
   container = document.createElement( "div" );
   document.body.appendChild( container );
-  camera = new THREE.PerspectiveCamera( 99, window.innerWidth / window.innerHeight, 0.1, 5000 );
+  camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 0.1, 3000 );
   // camera.position.z = 111;
-  camera.position.set( 0, 222, 222 );
+  camera.position.set( 0, 444, -844 );
   cameraTarget = new THREE.Vector3( 0, 0, 0 );
   camera.lookAt( cameraTarget );
 
@@ -34,7 +35,8 @@ function init() {
   // ASCII file
   var loader = new STLLoader();
   loader.load( "../stl/villa.stl", function ( geometry ) {
-    var material = new THREE.MeshPhongMaterial( { color: 0xAAAAAA, specular: 0x111111, shininess: 100 } );
+    var material = new THREE.MeshPhongMaterial( { color: 0xcccccc } );
+    // var material = new THREE.MeshBasicMaterial( { color: 0x44aa88 } );
     var mesh = new THREE.Mesh( geometry, material );
     // mesh.position.set( 15, 0, 0 );
     // mesh.rotation.set( - Math.PI / 2, 0, 0 );
@@ -43,24 +45,17 @@ function init() {
     // mesh.receiveShadow = true;
     scene.add( mesh );
   } );
-  // Binary files
-  // var material = new THREE.MeshPhongMaterial( { color: 0xAAAAAA, specular: 0x111111, shininess: 200 } );
-  // loader.load( "/stl/body.stl", function ( geometry ) {
-  //   var mesh2 = new THREE.Mesh( geometry, material );
-  //   mesh2.position.set( 0,0,0 );
-  //   // mesh.rotation.set( - Math.PI / 2, 0, 0 );
-  //   mesh2.scale.set( 0.05, 0.05, 0.05 );
-  //   // mesh.castShadow = true;
-  //   // mesh.receiveShadow = true;
-  //   scene.add( mesh2 );
-  // } );
+
 
   // Lights
-  scene.add( new THREE.HemisphereLight( 0x7777ff, 0xffffff, 0.95 ) );
+  // scene.add( new THREE.HemisphereLight( 0xbbbbbb ) );
 
-  var directionalLight1 = new THREE.DirectionalLight( 0xffffff, 0.8 );
-  directionalLight1.position.set( 33,33,33 );
-  scene.add( directionalLight1 );
+  var lit1 = new THREE.DirectionalLight( 0xddddff, 0.9 );
+  lit1.position.set( 55,33,55 );
+  scene.add( lit1 );
+  var lit2 = new THREE.DirectionalLight( 0xddddff, 0.9 );
+  lit2.position.set( -55,55,-55 );
+  scene.add( lit2 );
   // var directionalLight2 = new THREE.DirectionalLight( 0xffffff, 1 );
   // directionalLight2.position.set( -99,-99,99 );
   // scene.add( directionalLight2 );
@@ -97,3 +92,22 @@ function animate() {
 
 init();
 animate();
+
+function makeInstance( geometry, color, x, y ) {
+  const material = new THREE.MeshPhongMaterial( { color } );
+  const cube = new THREE.Mesh( geometry, material );
+  cube.position.x = x;
+  cube.position.y = y;
+  return cube;
+}
+function repo( e ) {
+  e.preventDefault();
+
+  mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
+  mouse.y = -( e.clientY / window.innerHeight ) * 2 + 1;
+  const geometry = new THREE.BoxGeometry( 22, 22, 22 );
+  let inst = makeInstance( geometry, 0x44aa88, mouse.x,mouse.y );
+  scene.add( inst );
+}
+
+window.addEventListener( "click", repo );

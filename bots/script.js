d86e5bb69d600d9738f9596f39d1e9660de165d3
sethb <getsetbro@users.noreply.github.com>
2020-04-22 10:48:55 -0400

    bots

--- bots/script.js
+++ bots/script.js
@@ -1,164 +1,177 @@
-
-// import * as THREE from "../build/three.module.js";
-import * as THREE from "../web_modules/three.js";
-
-// import Stats from "./jsm/libs/stats.module.js";
-// import { FirstPersonControls } from "../jsm/controls/FirstPersonControls.js";
+import * as THREE from "../build/three.module.js";
+import { OrbitControls } from "../jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "../jsm/loaders/GLTFLoader.js";
-// import { ShadowMapViewer } from "../jsm/utils/ShadowMapViewer.js";
-var SHADOW_MAP_WIDTH = 2048;
-var SHADOW_MAP_HEIGHT = 1024;
-var SCREEN_WIDTH = window.innerWidth;
-var SCREEN_HEIGHT = window.innerHeight;
-var FLOOR = - 250;
-var camera;
-var scene;
-var renderer;
-var NEAR = 10;
-var FAR = 3000;
-var mixer;
-var morphs = [];
-var light;
-// var lightShadowMapViewer;
-var clock = new THREE.Clock();
-// var showHUD = false;
-init();
-animate();
 
-function init() {
-  // CAMERA
-  camera = new THREE.PerspectiveCamera( 77, SCREEN_WIDTH / SCREEN_HEIGHT, NEAR, FAR );
-  camera.position.set( 10, 10, 1200 );
-  // SCENE
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color( 0x555555 );
-  // scene.fog = new THREE.Fog( 0x59472b, 1000, FAR );
-  // LIGHTS
-  var ambient = new THREE.AmbientLight( 0x999999 );
-  scene.add( ambient );
-  light = new THREE.SpotLight( 0xffffff, 1, 0, Math.PI / 5, 0.3 );
-  light.position.set( 0, 1500, 1000 );
-  light.target.position.set( 0, 0, 0 );
-  light.castShadow = true;
-  light.shadow.camera.near = 1200;
-  light.shadow.camera.far = 2500;
-  light.shadow.bias = 0.0001;
-  light.shadow.mapSize.width = SHADOW_MAP_WIDTH;
-  light.shadow.mapSize.height = SHADOW_MAP_HEIGHT;
-  scene.add( light );
-  // createHUD();
-  createScene();
-  // RENDERER
-  renderer = new THREE.WebGLRenderer( { antialias: true } );
-  renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
-  document.body.appendChild( renderer.domElement );
-  renderer.outputEncoding = THREE.sRGBEncoding;
-  renderer.autoClear = false;
-  //
-  renderer.shadowMap.enabled = true;
-  renderer.shadowMap.type = THREE.PCFShadowMap;
-  // CONTROLS
-  // controls = new FirstPersonControls( camera, renderer.domElement );
-  // controls.lookSpeed = 0.0125;
-  // controls.movementSpeed = 500;
-  // controls.noFly = false;
-  // controls.lookVertical = true;
-  // controls.lookAt( scene.position );
-  // STATS
-  // stats = new Stats();
-  //container.appendChild( stats.dom );
-  //
-  window.addEventListener( "resize", onWindowResize, false );
-  // window.addEventListener( "keydown", onKeyDown, false );
-}
-function onWindowResize() {
-  SCREEN_WIDTH = window.innerWidth;
-  SCREEN_HEIGHT = window.innerHeight;
-  camera.aspect = SCREEN_WIDTH / SCREEN_HEIGHT;
-  camera.updateProjectionMatrix();
-  renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
-  // controls.handleResize();
-}
-// function onKeyDown( event ) {
-//   switch ( event.keyCode ) {
-//   case 84:	/*t*/
-//     showHUD = ! showHUD;
-//     break;
-//   }
-// }
-// function createHUD() {
-//   lightShadowMapViewer = new ShadowMapViewer( light );
-//   lightShadowMapViewer.position.x = 10;
-//   lightShadowMapViewer.position.y = SCREEN_HEIGHT - ( SHADOW_MAP_HEIGHT / 4 ) - 10;
-//   lightShadowMapViewer.size.width = SHADOW_MAP_WIDTH / 4;
-//   lightShadowMapViewer.size.height = SHADOW_MAP_HEIGHT / 4;
-//   lightShadowMapViewer.update();
-// }
-function createScene( ) {
-  // GROUND
-  var geometry = new THREE.PlaneBufferGeometry( 100, 100 );
-  var planeMaterial = new THREE.MeshPhongMaterial( { color: "darkgreen" } );
-  var ground = new THREE.Mesh( geometry, planeMaterial );
-  ground.position.set( 0, FLOOR, 0 );
-  ground.rotation.x = - Math.PI / 2;
-  ground.scale.set( 100, 100, 100 );
-  ground.castShadow = false;
-  ground.receiveShadow = true;
-  scene.add( ground );
+// import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r115/build/three.module.js';
+// import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r115/examples/jsm/controls/OrbitControls.js';
+// import {GLTFLoader} from 'https://threejsfundamentals.org/threejs/resources/threejs/r115/examples/jsm/loaders/GLTFLoader.js';
 
-  mixer = new THREE.AnimationMixer( scene );
+function main() {
+  const canvas = document.querySelector( "#c" );
+  const renderer = new THREE.WebGLRenderer( { canvas } );
 
-  var loader = new GLTFLoader();
-  loader.load( "../glb/Horse.glb", function ( gltf ) {
+  const fov = 45;
+  const aspect = 2;  // the canvas default
+  const near = 0.1;
+  const far = 1000;
+  const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+  camera.position.set( 99, 444, -99 );
 
-    addMorph( gltf, -1300, 100 );
-    addMorph( gltf, -1400, 200 );
-    addMorph( gltf, -1500, 300 );
-  } );
+  const controls = new OrbitControls( camera, canvas );
+  controls.target.set( 0, 5, 0 );
+  controls.update();
 
-}
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color( "pink" );
 
-function addMorph( gltf, x, z ) {
-  var mesh = gltf.scene.children[ 0 ];
-  var clip = gltf.animations[ 0 ];
-  mesh = mesh.clone();
-  mesh.material = mesh.material.clone();
+  {
+    const planeSize = 444;
 
-  mesh.speed = 550;
-  mixer.clipAction( clip, mesh ).
-    setDuration( 1 ).
-  // to shift the playback out of phase:
-    // startAt( - duration * Math.random() ).
-    play();
-  mesh.position.set( x, FLOOR, z );
-  mesh.rotation.y = Math.PI / 2;
-  mesh.castShadow = true;
-  mesh.receiveShadow = true;
-  scene.add( mesh );
-  morphs.push( mesh );
-}
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load( "https://threejsfundamentals.org/threejs/resources/images/checker.png" );
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.magFilter = THREE.NearestFilter;
+    const repeats = planeSize / 2;
+    texture.repeat.set( repeats, repeats );
 
-function animate() {
-  requestAnimationFrame( animate );
-  render();
-  // stats.update();
-}
-function render() {
-  var delta = clock.getDelta();
-  mixer.update( delta );
-  for ( var i = 0; i < morphs.length; i ++ ) {
-    var morph = morphs[ i ];
-    morph.position.x += morph.speed * delta;
-    if ( morph.position.x > 2000 ) {
-      morph.position.x = -1222;
-    }
+    const planeGeo = new THREE.PlaneBufferGeometry( planeSize, planeSize );
+    const planeMat = new THREE.MeshPhongMaterial( {
+      map: texture,
+      side: THREE.DoubleSide
+    } );
+    const mesh = new THREE.Mesh( planeGeo, planeMat );
+    mesh.rotation.x = Math.PI * -.5;
+    scene.add( mesh );
   }
-  // controls.update( delta );
-  renderer.clear();
-  renderer.render( scene, camera );
-  // Render debug HUD with shadow map
-  // if ( showHUD ) {
-  //   lightShadowMapViewer.render( renderer );
-  // }
+
+  {
+    const skyColor = 0xB1E1FF;  // light blue
+    const groundColor = 0xB97A20;  // brownish orange
+    const intensity = 1;
+    const light = new THREE.HemisphereLight( skyColor, groundColor, intensity );
+    scene.add( light );
+  }
+
+  {
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight( color, intensity );
+    light.position.set( 5, 10, 222 );
+    scene.add( light );
+    scene.add( light.target );
+  }
+
+  function frameArea( sizeToFitOnScreen, boxSize, boxCenter, camera ) {
+    const halfSizeToFitOnScreen = sizeToFitOnScreen * 0.5;
+    const halfFovY = THREE.MathUtils.degToRad( camera.fov * .5 );
+    const distance = halfSizeToFitOnScreen / Math.tan( halfFovY );
+    // compute a unit vector that points in the direction the camera is now
+    // in the xz plane from the center of the box
+    const direction = ( new THREE.Vector3() )
+      .subVectors( camera.position, boxCenter )
+      .multiply( new THREE.Vector3( 1, 0, 1 ) )
+      .normalize();
+
+    // move the camera to a position distance units way from the center
+    // in whatever direction the camera was from the center already
+    camera.position.copy( direction.multiplyScalar( distance ).add( boxCenter ) );
+
+    // pick some near and far values for the frustum that
+    // will contain the box.
+    camera.near = boxSize / 100;
+    camera.far = boxSize * 100;
+
+    camera.updateProjectionMatrix();
+
+    // point the camera to look at the center of the box
+    camera.lookAt( boxCenter.x, boxCenter.y, boxCenter.z );
+  }
+
+  {
+    // const gltfLoader = new GLTFLoader();
+    // gltfLoader.load( "https://threejsfundamentals.org/threejs/resources/models/cartoon_lowpoly_small_city_free_pack/scene.gltf", ( gltf ) => {
+    //   const root = gltf.scene;
+    //   scene.add( root );
+
+    //   // compute the box that contains all the stuff
+    //   // from root and below
+    //   const box = new THREE.Box3().setFromObject( root );
+
+    //   const boxSize = box.getSize( new THREE.Vector3() ).length();
+    //   const boxCenter = box.getCenter( new THREE.Vector3() );
+
+    //   // set the camera to frame the box
+    //   frameArea( boxSize * 0.5, boxSize, boxCenter, camera );
+
+    //   // update the Trackball controls to handle the new size
+    //   controls.maxDistance = boxSize * 10;
+    //   controls.target.copy( boxCenter );
+    //   controls.update();
+    // } );
+
+
+    // var loader = new GLTFLoader();
+    // loader.load( "./monsoon/scene.gltf", function ( gltf ) {
+    //   const root = gltf.scene;
+
+    //   root.position.set( -99,0,-99 );
+    //   // root.rotation.set( - Math.PI / 2, Math.PI / 2, 0 );
+    //   // root.scale.set( 0.1, 0.1, 0.1 );
+    //   // root.castShadow = true;
+    //   // root.receiveShadow = true;
+    //   scene.add( root );
+    // }, undefined, function ( error ) {console.error( error );} );
+
+    var loader2 = new GLTFLoader();
+    loader2.load( "./end_game/scene.gltf", function ( gltf ) {
+      const root = gltf.scene;
+      root.position.set( -2,0,-2 );
+      // root.scale.set( 0.1, 0.1, 0.1 );
+
+      scene.add( root );
+
+      // compute the box that contains all the stuff
+      // from root and below
+      const box = new THREE.Box3().setFromObject( root );
+
+      const boxSize = box.getSize( new THREE.Vector3() ).length();
+      const boxCenter = box.getCenter( new THREE.Vector3() );
+
+      // set the camera to frame the box
+      frameArea( boxSize * 0.95, boxSize, boxCenter, camera );
+
+      // update the Trackball controls to handle the new size
+      controls.maxDistance = boxSize * 10;
+      controls.target.copy( boxCenter );
+      controls.update();
+    } );
+  }
+
+  function resizeRendererToDisplaySize( renderer ) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if ( needResize ) {
+      renderer.setSize( width, height, false );
+    }
+    return needResize;
+  }
+
+  function render() {
+    if ( resizeRendererToDisplaySize( renderer ) ) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+
+    renderer.render( scene, camera );
+
+    requestAnimationFrame( render );
+  }
+
+  requestAnimationFrame( render );
 }
+
+main();
